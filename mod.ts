b31d1bf5d9d16a339609e503bb968d3238b5f60e
Tamim Arafat <tamim.arafat@gmail.com>
2021-02-14 18:05:34 +0600

    Improved types

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
 import dew from "https://dev.jspm.io/cheerio@1.0.0-rc.3";
 import { CheerioAPI } from "./types.ts";
 export const cheerio: CheerioAPI = dew as CheerioAPI;
+export * from "./types.ts";
--- test.ts
+++ test.ts
@@ -1,4 +1,5 @@
 import { cheerio } from "./mod.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 
 export const getPickupLine = () =>
   fetch("http://www.pickuplinegen.com")
@@ -8,4 +9,20 @@ export const getPickupLine = () =>
 
 Deno.test("Cheerio test", async () => {
   await getPickupLine().then(console.log);
+});
+
+Deno.test("Data attribute types are correct", () => {
+  const div = cheerio.load(
+    `<div data-number="3" data-string="this is a string" data-boolean="true" data-empty>test div</div>`
+  )("div");
+
+  const dataNumber = div.data("number");
+  const dataString = div.data("string");
+  const dataBool = div.data("boolean");
+  const dataEmpty = div.data("empty");
+
+  assertEquals(typeof dataNumber, "number");
+  assertEquals(typeof dataString, "string");
+  assertEquals(typeof dataBool, "boolean");
+  assertEquals(typeof dataEmpty, "string");
 });
