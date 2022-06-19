3d437c67daa58b8be2d13f49825e65b77b040a9d
UwUssimo Robinson <uwussimo@icloud.com>
2022-06-19 19:19:24 +0500

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import { dew } from "https://dev.jspm.io/npm:cheerio@1.0.0-rc.3/index.dew.js";
+import { dew } from "https://dev.jspm.io/npm:cheerio@1.0.0-rc.10/index.dew.js";
 import { CheerioAPI } from "./types.ts";
 export const cheerio: CheerioAPI = dew() as CheerioAPI;
 export * from "./types.ts";
